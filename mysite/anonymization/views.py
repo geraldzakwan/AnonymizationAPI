3d2f310a2e49fe220ff20e54dd751b0bb6fbcf2f
geraldzakwan <geraldi.dzakwan@gmail.com>
2017-06-23 15:19:50 +0900

    handle GET and POST

--- mysite/anonymization/views.py
+++ mysite/anonymization/views.py
@@ -4,7 +4,16 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 
+@csrf_exempt
 def index(request):
-    return HttpResponse("Welcome to anonymization API")
+    if (request.method == 'GET'):
+        text = 'Welcome to English message anonymization API.' + '\n\n' + 'Sample JSON message: {'+ '\n' + '    message: "My friend, Ibrahim, comes from France."' + '\n' +'}'
+        return HttpResponse(text)
+    elif (request.method == 'POST'):
+        text = request.POST.get('message')
+        if (text == None):
+            text = 'Wrong JSON format sent.' + '\n\n' + 'Sample JSON message: {'+ '\n' + '    message: "My friend, Ibrahim, comes from France."' + '\n' +'}'
+        return HttpResponse(text)
